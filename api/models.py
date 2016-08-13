454317827b31b89ee7d764abf905988f09311004
Juan L. Sánchez <juanlu-28a@hotmail.com>
2016-08-13 13:56:46 +0200

    Fix bug in the times creation and update
    When a recipe was creating, "updated" and "created" fields were exchanged, then when a object is updated the field "created" was modified.
    The bug has been modified

--- api/models.py
+++ api/models.py
@@ -4,8 +4,8 @@ from django.db import models
 class Recipe(models.Model):
     name = models.CharField(blank=False, unique=True, max_length=100)
     description = models.TextField(default='')
-    created = models.DateTimeField(auto_now=True)
-    updated = models.DateTimeField(auto_now_add=True)
+    created = models.DateTimeField(auto_now_add=True)
+    updated = models.DateTimeField(auto_now=True)
     owner = models.ForeignKey('auth.User', related_name='recipes')
 
     class Meta:
